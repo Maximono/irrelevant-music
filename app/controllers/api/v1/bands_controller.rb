d88bb625c6b660ea3b358bec7f63e95750661f20
Maksym Tesliuk <maxym.tesliuk@gmail.com>
2020-08-13 13:27:43 +0300

    Extend bands list api enpoint to perform sorting and pagination

--- app/controllers/api/v1/bands_controller.rb
+++ app/controllers/api/v1/bands_controller.rb
@@ -3,8 +3,20 @@ module Api
     class BandsController < ApplicationController
 
       def index
-        bands = Band.all.page(params[:page]).per(5)
+        bands = Band.page(params[:page]).per(5)
+        bands = bands.order("#{sort_column} #{sort_direction}") if params[:sort_column]
         render json: BandSerializer.new(bands).serialized_json
+      end
+
+      private
+
+      def sort_column
+        columns = %w[name genres albums]
+        params[:sort_column] if columns.include?(params[:sort_column])
+      end
+
+      def sort_direction
+        params[:sort_direction] == "desc" ? "desc" : "asc"
       end
     end
   end
--- spec/requests/api/v1/bands_requests_spec.rb
+++ spec/requests/api/v1/bands_requests_spec.rb
@@ -12,5 +12,27 @@ describe 'Bands api' do
       expect(response).to have_http_status(:success)
       expect(parsed_response[:data].size).to eq(1)
     end
+
+    it 'returns paginated page of bands list' do
+      FactoryBot.create_list(:band, 10)
+
+      get api_v1_bands_path(params: { page: '2'})
+      parsed_response = JSON.parse(response.body).deep_symbolize_keys
+
+      expect(response).to have_http_status(:success)
+      expect(parsed_response[:data].size).to eq(5)
+    end
+
+    it 'returns bands list sorted by name' do
+      %w[Anthrax Slayer].map do |name|
+        FactoryBot.create(:band, name: name)
+      end
+
+      get api_v1_bands_path(params: { sort_column: 'name', sort_direction: 'desc' })
+      parsed_response = JSON.parse(response.body).deep_symbolize_keys
+
+      expect(response).to have_http_status(:success)
+      expect(parsed_response[:data].first[:attributes][:name]).to eq('Slayer')
+    end
   end
 end
