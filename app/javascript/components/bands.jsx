9447f0ac46f8705b613d05888867ae40b9627d5c
Maksym Tesliuk <maxym.tesliuk@gmail.com>
2020-08-14 12:00:52 +0300

    Add the ability to sort table by column

--- app/javascript/components/bands.jsx
+++ app/javascript/components/bands.jsx
@@ -5,6 +5,23 @@ class Bands extends React.Component {
   constructor(props) {
     super(props);
     this.state = { bands: [], sortingColumn: 'name', sortingDirection: 'asc'};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    const self = this;
+    const eventName = event.currentTarget.name;
+    if (event.currentTarget.name === this.state.sortingColumn) {
+
+      const newSortingDirection = (this.state.sortingDirection === 'asc') ? 'desc' : 'asc';
+      getBands(this.state.sortingColumn, newSortingDirection).then(function (data) {
+        self.setState({bands: data.bands, sortingDirection: newSortingDirection})
+      });
+    } else {
+      getBands(eventName, this.state.sortingDirection).then(function (data) {
+        self.setState({bands: data.bands, sortingColumn: eventName})
+      });
+    }
   }
 
   componentDidMount() {
@@ -17,18 +34,28 @@ class Bands extends React.Component {
   render() {
     return (
       <table>
-        <tr>
-          <th>Name</th>
-          <th>Genres</th>
-          <th>Albums</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>
+              <button name='name' onClick={this.handleClick}>Name</button>
+            </th>
+            <th>
+              <button name='genres' onClick={this.handleClick}>Genres</button>
+            </th>
+            <th>
+              <button name='albums' onClick={this.handleClick}>Albums</button>
+            </th>
+          </tr>
+        </thead>
         {this.state.bands.map(function(band) {
           return (
-            <tr key={band.id}>
-              <td>{band.attributes.name}</td>
-              <td>{band.attributes.genres}</td>
-              <td>{band.attributes.albums}</td>
-            </tr>
+            <tbody>
+              <tr key={band.id}>
+                <td>{band.attributes.name}</td>
+                <td>{band.attributes.genres}</td>
+                <td>{band.attributes.albums}</td>
+              </tr>
+            </tbody>
           )
         }) }
       </table>
